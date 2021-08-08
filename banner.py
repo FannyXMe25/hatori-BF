393711f1e8a4e79ff8518d91340a8891932421f5
Fanny25 <88589620+FannyXMe25@users.noreply.github.com>
2021-08-08 08:07:09 +0700

    Create banner.py

--- banner.py
+++ banner.py
@@ -1,33 +1,25 @@
-########                  #
-                      #################            #
-                   ######################         #
-                  #########################      #
-                ############################
-               ##############################
-               ###############################
-              ###############################
-              ##############################
-                              #    ########   #
-                 ##        ###        ####   ##
-                                      ###   ###
-                                    ####   ###
-               ####          ##########   ####
-               #######################   ####
-                 ####################   ####
-                  ##################  ####
-                    ############      ##
-                       ########        ###
-                      #########        #####
-                    ############      ######
-                   ########      #########
-                     #####       ########
-                       ###       #########
-                      ######    ############
-                     #######################
-                     #   #   ###  #   #   ##
-                     ########################
-                      ##     ##   ##     ##
-                 gitHub : https://github.com/FannyXMe25/hatori-BF
-   =[ coding by : My Fanny ]
-   
-   
+.-/+osssso+/-`
+                `/sdNMMMMMMMMMMMMMMNds/`
+             .odMMMMMMMMMMMMMMMMMMMMMMMMdo.
+           :hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh:
+         -dMMMNmMMMMMMMNNNNNNNNNNMMMMMMMmNMMMd-
+       `sMNNs-oMMMMNNNNNNNNNNNNNNNNNNMMMMo-sNNMs
+      `dM+s-:sMMMNNNNNNMMNMNNMNMMNNNNNNMMMs-:s+Md`
+     `mho.h/:dMNNMMMNMMNmNoso-dmNMMNMMMNNMd:/d`ohm`
+     hs//::oMMNMMMMNMMMMNMmmo+NNMMMMNMMMMNMNo:-+:yh
+    /M--y+:hMNNNNNNNMMMNNMMmmMMNMMMMNNNNNNNMh:oy.:M:
+    dd+./.hMNMMMMMNMMMNNNNNooNNNNNMMMNMMMMMNMy./.odh
+   `M-m-/yyMNMMMMMNMMMMNMNdssdNMNMMMMNMMMMMNMyy/-m-N
+   `M:-ho`NMNNNNNNNNNmho-sN:/Ms-shmNNNNNNNNNMm`sh-/M`
+   `Mh`+ sdMNMMMMMh`     NMosMN     `dMMMMMNMdo`/`hN
+    d/h/.m.MNMMMMM+      yM--My      oMMMMMNM.m`/h/h
+    /d`/ss mNNNNNN-      `d.-h`      -NNNNNNd ss/`d:
+     hm/`+ d:mNMMN                    NMMNd:h`+`/mh
+     `doss/++.mNNh                    hNNm`o//ssod`
+      `ds.:+y-:sso                    osy:-y+--sd`
+        sMy+/:./+                      +/.:/+yMs
+         -dyo/////.```            ```./////ohh-
+           :hMhoosss/`            `/sssoohMh:
+             .ohysoos.            .soosyh+`
+                `/sdN`            `Nds/`
+...::::[ Powered by https://github.com/FannyXMe25 ]::::...
