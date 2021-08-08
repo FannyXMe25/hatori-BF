32c26430167e73dfe11623c231012f2e2a9d1824
Fanny25 <88589620+FannyXMe25@users.noreply.github.com>
2021-08-08 20:31:03 +0700

    Create banner.py

--- banner.py
+++ banner.py
@@ -1,25 +1,8 @@
-.-/+osssso+/-`
-                `/sdNMMMMMMMMMMMMMMNds/`
-             .odMMMMMMMMMMMMMMMMMMMMMMMMdo.
-           :hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh:
-         -dMMMNmMMMMMMMNNNNNNNNNNMMMMMMMmNMMMd-
-       `sMNNs-oMMMMNNNNNNNNNNNNNNNNNNMMMMo-sNNMs
-      `dM+s-:sMMMNNNNNNMMNMNNMNMMNNNNNNMMMs-:s+Md`
-     `mho.h/:dMNNMMMNMMNmNoso-dmNMMNMMMNNMd:/d`ohm`
-     hs//::oMMNMMMMNMMMMNMmmo+NNMMMMNMMMMNMNo:-+:yh
-    /M--y+:hMNNNNNNNMMMNNMMmmMMNMMMMNNNNNNNMh:oy.:M:
-    dd+./.hMNMMMMMNMMMNNNNNooNNNNNMMMNMMMMMNMy./.odh
-   `M-m-/yyMNMMMMMNMMMMNMNdssdNMNMMMMNMMMMMNMyy/-m-N
-   `M:-ho`NMNNNNNNNNNmho-sN:/Ms-shmNNNNNNNNNMm`sh-/M`
-   `Mh`+ sdMNMMMMMh`     NMosMN     `dMMMMMNMdo`/`hN
-    d/h/.m.MNMMMMM+      yM--My      oMMMMMNM.m`/h/h
-    /d`/ss mNNNNNN-      `d.-h`      -NNNNNNd ss/`d:
-     hm/`+ d:mNMMN                    NMMNd:h`+`/mh
-     `doss/++.mNNh                    hNNm`o//ssod`
-      `ds.:+y-:sso                    osy:-y+--sd`
-        sMy+/:./+                      +/.:/+yMs
-         -dyo/////.```            ```./////ohh-
-           :hMhoosss/`            `/sssoohMh:
-             .ohysoos.            .soosyh+`
-                `/sdN`            `Nds/`
+|>>>>>>>>>>                                    <<<<<<<<<<|
 ...::::[ Powered by https://github.com/FannyXMe25 ]::::...
+|>>>>>>>>>>         [ WELCOME EVERYBODY ]      <<<<<<<<<<|
+
+╔══╦╗────╔╗─╔╗╔╗
+╚╗╔╣╚╦═╦═╣╚╗║╚╝╠═╦╦╗
+─║║║║║╬║║║╩║╚╗╔╣║║║║
+─╚╝╚╩╩╩╩╩╩╩╝─╚╝╚═╩═╝
