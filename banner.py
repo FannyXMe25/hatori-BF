552a6e7cf6481be89ea587422be2e96dae5a2cf9
Fanny25 <88589620+FannyXMe25@users.noreply.github.com>
2021-08-09 14:40:24 +0700

    Create banner.py

--- banner.py
+++ banner.py
@@ -1,13 +1,20 @@
      ...。☆。。☆。。☆。。☆。。☆。 。
      。☆。。☆。♫♫。☆。☆♫。 。☆。。。
-     ●▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬●
-         •>>>FACEBOOK CRACK<<<•
-     ●▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬●
-     ...。☆。。☆。。☆。。☆。。☆。 。
-     。☆。。☆。♫♫。☆。☆♫。 。☆。。。
-
-
-
+    ●▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬●
+          •>>>FACEBOOK CRACK<<<•
+    ●▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬●
+`M:-ho`NMNNNNNNNNNmho-sN:/Ms-shmNNNNNNNNNMm`sh-/M`
+`Mh`+ sdMNMMMMMh`     NMosMN     `dMMMMMNMdo`/`hN
+ d/h/.m.MNMMMMM+      yM--My      oMMMMMNM.m`/h/h
+ /d`/ss mNNNNNN-      `d.-h`      -NNNNNNd ss/`d:
+  hm/`+ d:mNMMN                    NMMNd:h`+`/mh
+  `doss/++.mNNh                    hNNm`o//ssod`
+   `ds.:+y-:sso                    osy:-y+--sd`
+     sMy+/:./+                      +/.:/+yMs
+      -dyo/////.```            ```./////ohh-
+        :hMhoosss/`            `/sssoohMh:
+          .ohysoos.            .soosyh+`
+             `/sdN`            `Nds/`
 
 
 ●▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬●
