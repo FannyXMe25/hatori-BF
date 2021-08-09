e320bf69c046685e3b277a70215b29797ae21765
Fanny25 <88589620+FannyXMe25@users.noreply.github.com>
2021-08-09 22:15:40 +0700

    Create banner.py

--- banner.py
+++ banner.py
@@ -1,13 +1,10 @@
-     ●▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬●
-         •>>>FACEBOOK CRACK<<<•
-     ●▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬●
-
-
+●▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬●
+     >WELCOME<
+●▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬●
 
 ●▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬●
-
-Author  : Fanny
-Github  : https://github.com/FannyXMe25
-Facebook: https://www.facebook.com/erpann.botan
-
+{*} Author     : Fanny
+{*} Author Ori : Dapunta Khurayra
+{*} Github     : https://github.com/FannyXMe25
+{*} Facebook   : https://www.facebook.com/erpann.botan
 ●▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬●
