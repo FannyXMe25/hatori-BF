2cf60afab230bbc236700e4ace4c4868cddd3026
Fanny25 <88589620+FannyXMe25@users.noreply.github.com>
2021-08-09 15:52:54 +0700

    Create banner.py

--- banner.py
+++ banner.py
@@ -1,21 +1,16 @@
+●▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬●
+     •>>>𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞 𝗖𝗥𝗔𝗖𝗞<<<•
+●▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬●
 
-          ●▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬●
-               •>>>𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞 𝗖𝗥𝗔𝗖𝗞<<<•
-          ●▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬●
 
-`M:-ho`NMNNNNNNNNNmho-sN:/Ms-shmNNNNNNNNNMm`sh-/M`
-`Mh`+ sdMNMMMMMh`     NMosMN     `dMMMMMNMdo`/`hN
- d/h/.m.MNMMMMM+      yM--My      oMMMMMNM.m`/h/h
- /d`/ss mNNNNNN-      `d.-h`      -NNNNNNd ss/`d:
-  hm/`+ d:mNMMN                    NMMNd:h`+`/mh
-  `doss/++.mNNh                    hNNm`o//ssod`
-   `ds.:+y-:sso                    osy:-y+--sd`
-     sMy+/:./+                      +/.:/+yMs
-      -dyo/////.```            ```./////ohh-
-        :hMhoosss/`            `/sssoohMh:
-          .ohysoos.            .soosyh+`
-             `/sdN`            `Nds/`
-
+  ▛    ▜
+█████████
+█▄█████▄█
+█▼▼▼▼▼█
+██      ██
+█▲▲▲▲▲█
+█████████  
+ ██    ██
 
 
 ●▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬●●▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬●
